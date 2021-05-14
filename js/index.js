8243696fe669f74e951ac6340084c6d8629eb76e
jepore97 <jepore97@gmail.com>
2021-05-14 14:48:07 -0500

    new cam

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
+const nombreDias = [
+    'domingo',
+    'lunes',
+    'martes',
+    'miércoles',
+    'jueves',
+    'viernes',
+    'sábado',
+    'domingo',
+    'lunes',
+    'martes',
+    'miércoles',
+  ];
+
+
 function ajax_get(url, callback) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
@@ -28,8 +43,56 @@ ajax_get('http://api.openweathermap.org/data/2.5/weather?q=Bogota&appid=b9051013
 
 
 ajax_get('http://api.openweathermap.org/data/2.5/forecast?q=bogota&appid=b90510135d2590bea2fb457e60b3abff', function(data) {
+    var diaActual = new Date().getDate();
+    console.log(nombreDias[new Date().getDay()+3]);
+    var maxd1 = 0;
+    var mind1=1000;
+    var maxd2 = 0;
+    var mind2=1000;
+    var maxd3 = 0;
+    var mind3=1000;
     for (var i=0; i < data["list"].length; i++) {
-        console.log(data["list"][i].dt_txt)
+        var dias=Math.round(data["list"][i].dt_txt.substr(-11,2));
+        
+        if((dias-diaActual)==1) {
+            if (data["list"][i]['main'].temp>maxd1) {
+                maxd1=Math.round(data["list"][i]['main'].temp)
+            }
+            if (data["list"][i]['main'].temp<mind1) {
+                mind1=Math.round(data["list"][i]['main'].temp)
+            }
+            document.getElementById("txt-temp-item1").innerHTML=maxd1+"° / "+mind1+"°"
+            document.getElementById("txt-dia-item1").innerHTML=nombreDias[new Date().getDay()+1]
+            
+            document.getElementById("txt-desc-item1").innerHTML=data["list"][i]['weather'][0].main
+            document.getElementById("icon-item1").src="http://openweathermap.org/img/w/" + data["list"][i]['weather'][0].icon + ".png";
+        }else if((dias-diaActual)==2) {
+            if (data["list"][i]['main'].temp>maxd2) {
+                maxd2=Math.round(data["list"][i]['main'].temp)
+            }
+            if (data["list"][i]['main'].temp<mind2) {
+                mind2=Math.round(data["list"][i]['main'].temp)
+            }
+            document.getElementById("txt-temp-item2").innerHTML=maxd2+"° / "+mind2+"°"
+            document.getElementById("txt-dia-item2").innerHTML=nombreDias[new Date().getDay()+2]
+            document.getElementById("icon-item2").src="http://openweathermap.org/img/w/" + data["list"][i]['weather'][0].icon + ".png";
+            document.getElementById("txt-desc-item2").innerHTML=data["list"][i]['weather'][0].main
+            
+        }
+        else if((dias-diaActual)==3) {
+            if (data["list"][i]['main'].temp>maxd3) {
+                maxd3=Math.round(data["list"][i]['main'].temp)
+            }
+            if (data["list"][i]['main'].temp<mind3) {
+                mind3=Math.round(data["list"][i]['main'].temp)
+            }
+            document.getElementById("txt-temp-item3").innerHTML=maxd3+"° / "+mind3+"°"
+            document.getElementById("txt-dia-item3").innerHTML=nombreDias[new Date().getDay()+3]
+            document.getElementById("icon-item3").src="http://openweathermap.org/img/w/" + data["list"][i]['weather'][0].icon + ".png";
+            document.getElementById("txt-desc-item3").innerHTML=data["list"][i]['weather'][0].main
+            
+        }
+
     }
    
 });
